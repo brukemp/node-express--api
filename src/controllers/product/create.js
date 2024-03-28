e75f49ee6df902429421a3067ba562536f2a12a7
Bruna Kempinsk <kempinskb@gmail.com>
2024-03-28 18:29:08 -0300

    att

--- src/controllers/product/create.js
+++ src/controllers/product/create.js
@@ -2,6 +2,14 @@ import productModel from "../../models/productModel.js"
 
 const create = (req, res) => {
 	const product = req.body
+	const validateData = productModel.validateCreate(product)
+	if(!validateData.success){
+		return res.status(400).json({
+			error: "Dados Inválidos!",
+			fields: validateData.error.flatten().fieldErrors
+		})
+	}
+
 	const result = productModel.create(product)
 	res.json({
 		success: "Produto adicionado com sucesso!",
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,4 +1,43 @@
 import {products} from '../db-memory/products.js'
+import {z} from 'zod'
+
+const productSchema = z.object({
+	id: z
+		.number({
+			invalid_type_error: 'O id deve ser numérico.',
+			required_error: 'Id obrigatório.'
+		}),
+		name: z
+		.string({
+			invalid_type_error: 'O nome deve ser uma string.',
+			required_error: 'Nome obrigatório.'
+		})
+		.min(3, {message: 'O nome do usuário deve ter ao menos 3 lestras.'})
+		.max(200, {message: 'O nome do usuário deve ter no máximo 200 caracteres.'}),
+		value: z
+			.number({
+				invalid_type_error: 'O preço deve ser um número',
+				required_error: 'Preço obrigatório.'
+			}),
+		category: z
+			.string({
+				invalid_type_error: 'A categoria deve ser uma string',
+				required_error: 'Categoria obrigatória.'
+			})
+			.min(3,{message: 'A categoria deve ter mo mínimo 3 caracteres'})
+			.max(50,{message: 'A categoria deve ter mo máximo 50 caracteres'}),
+		photo: z
+			.string({
+				invalid_type_error: 'A foto deve ser uma string',
+				required_error: 'Foto obrigatório'
+			})
+			.url({message: 'Url da foto inválida.'})
+
+})
+const validateCreate = (product) =>{
+	const partialProductSchema = productSchema.partial({id: true})
+	return partialProductSchema.safeParse(product)
+}
 
 const list = () => {
     return products
@@ -29,4 +68,4 @@ const remove = (id) => {
     return products.filter(product => product.id !== id)
 }
 
-export default {list, create, edit, remove} 
+export default {list, create, edit, remove, validateCreate} 
